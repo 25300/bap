529bd8051ede7b18f6175ea9e34a5ee72093ae66
25300 <34769565+25300@users.noreply.github.com>
2017-12-22 15:42:13 +0100

    Update sendmail.php

--- opdracht5/sendmail.php
+++ opdracht5/sendmail.php
@@ -4,7 +4,8 @@ $to = '25300@ma-web.nl';
 $from = $_POST['from'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+$headers = 'From: ' . $_POST['from'];
 
-mail($to, $subject, $message);
+mail($to, $subject, $message, $headers);
 
 echo 'Uw bericht is verzonden';
